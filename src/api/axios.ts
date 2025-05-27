55fa21268146dd5f54f35062b3b2af6beb692383
Sathiel13 <ramirezsath@gmail.com>
2025-05-27 16:16:22 -0600

    Correccion AXIOS

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
+
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, // usa la variable de entorno
+    baseURL: import.meta.env.VITE_API_URL, // Correcto para Vite
     withCredentials: true,
 });
 
